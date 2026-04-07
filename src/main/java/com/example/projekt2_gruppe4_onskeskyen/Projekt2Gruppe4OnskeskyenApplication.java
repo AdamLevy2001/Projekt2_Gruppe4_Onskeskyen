7f2d869f71d265a9da8ac0a1255a8daf30a110ac
AdamLevy2001 <adle0001@stud.ek.dk>
2026-04-07 10:41:23 +0200

    Init Commit

---
+++ src/main/java/com/example/projekt2_gruppe4_onskeskyen/Projekt2Gruppe4OnskeskyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekt2_gruppe4_onskeskyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2Gruppe4OnskeskyenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt2Gruppe4OnskeskyenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projekt2_gruppe4_onskeskyen/Projekt2Gruppe4OnskeskyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt2_gruppe4_onskeskyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt2Gruppe4OnskeskyenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
