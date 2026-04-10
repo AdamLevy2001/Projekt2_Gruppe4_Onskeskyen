3a84adf772347cad2304dac74eaf6042687715c8
maho0012 <maho0012@stud.ek.dk>
2026-04-10 12:31:31 +0200

    New User class made.

---
+++ src/main/java/com/example/projekt2_gruppe4_onskeskyen/model/User.java
@@ -0,0 +1,19 @@
+package com.example.projekt2_gruppe4_onskeskyen.model;
+
+public class User {
+
+    int id;
+
+    String name;
+
+    String email;
+
+    String password;
+
+    public User(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
