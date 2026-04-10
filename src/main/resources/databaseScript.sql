6537f2d089f6fbfe5ad9d631677a58b35e19b729
MFWahlgren <mawa0006@stud.ek.dk>
2026-04-10 09:24:06 +0200

    Small changes to databaseScript.sql. id (Primary key) added to reservation table, wish_id (in reservation table) made UNIQUE. VARCHAR lengths changed

--- src/main/resources/databaseScript.sql
+++ src/main/resources/databaseScript.sql
@@ -6,32 +6,32 @@ USE Onskeskyen;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE ,
-    password VARCHAR(50) NOT NULL
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE wishlist (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE wish (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
+    name VARCHAR(100),
     description TEXT,
     price DECIMAL(10,2),
-    link VARCHAR(255),
+    link VARCHAR(500),
     wishlist_id INT,
     FOREIGN KEY (wishlist_id) REFERENCES wishlist(id) ON DELETE CASCADE
 );
 
 CREATE TABLE reservation (
-    user_id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    wish_id INT,
-    FOREIGN KEY (wish_id) REFERENCES wish(id) ON DELETE CASCADE,
-    PRIMARY KEY(user_id, wish_id)
+    wish_id INT UNIQUE NOT NULL,
+    FOREIGN KEY (wish_id) REFERENCES wish(id) ON DELETE CASCADE
 );
