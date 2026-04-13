8aad8b4b622ab9cf852b31d348094bcf1eac7268
MFWahlgren <mawa0006@stud.ek.dk>
2026-04-13 11:18:33 +0200

    Added showWishlists for a user functionality

--- src/main/java/com/example/projekt2_gruppe4_onskeskyen/service/WishlistService.java
+++ src/main/java/com/example/projekt2_gruppe4_onskeskyen/service/WishlistService.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class WishlistService {
     @Autowired
@@ -18,5 +20,9 @@ public class WishlistService {
 
         Wishlist wishlist = new Wishlist(name, (Integer) session.getAttribute("userId"));
         wishlistRepository.saveWishlist(wishlist);
+    }
+
+    public ArrayList<Wishlist> getAllWishlistByUserID(int userID){
+        return wishlistRepository.findWishlistsByUserId(userID);
     }
 }
