94916fac768d4707cdc59d50db52fc86cf9640b6
MFWahlgren <mawa0006@stud.ek.dk>
2026-04-17 11:05:50 +0200

    Added functionality to share wishlist

--- src/main/java/com/example/projekt2_gruppe4_onskeskyen/controller/WishlistShareController.java
+++ src/main/java/com/example/projekt2_gruppe4_onskeskyen/controller/WishlistShareController.java
@@ -2,37 +2,55 @@ package com.example.projekt2_gruppe4_onskeskyen.controller;
 
 import com.example.projekt2_gruppe4_onskeskyen.model.User;
 import com.example.projekt2_gruppe4_onskeskyen.model.Wishlist;
+import com.example.projekt2_gruppe4_onskeskyen.service.UserService;
 import com.example.projekt2_gruppe4_onskeskyen.service.WishlistService;
 import com.example.projekt2_gruppe4_onskeskyen.service.WishlistShareService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class WishlistShareController {
 
     @Autowired
-    WishlistShareService service;
+    WishlistShareService wishlistShareService;
 
     @Autowired
     WishlistService wishlistService;
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/wishlist/share")
-    public String getWishlistShare(HttpSession session) {
+    public String getWishlistShare(@RequestParam(required = false) String query,
+                                   @RequestParam(required = false) Integer wishlistId, Model model, HttpSession session) {
         User user = (User) session.getAttribute("loggedInUser");
 
         if(user == null){
             return "redirect:/login";
         }
+
+        if (query != null && !query.isEmpty()) {
+            List<User> searchResults = userService.searchUsers(query, user.getId());
+            model.addAttribute("users", searchResults);
+            model.addAttribute("query", query);
+        }
+
+        model.addAttribute("wishlistId", wishlistId);
+
         return "wishlistShare";
     }
 
     @PostMapping("/wishlist/share")
-    public String shareWishlist(@RequestParam int wishlistId,
-                                @RequestParam int userId, HttpSession session) {
+    public String shareWishlist(@RequestParam Integer wishlistId,
+                                @RequestParam Integer userId,
+                                HttpSession session) {
         User user = (User) session.getAttribute("loggedInUser");
 
         if (user == null) {
@@ -45,8 +63,12 @@ public class WishlistShareController {
             return "redirect:/";
         }
 
-        service.shareWishlist(wishlistId, userId);
+        try {
+            wishlistShareService.shareWishlist(wishlistId, userId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        return "redirect:/showWishlists";
+        return "redirect:/";
     }
 }
